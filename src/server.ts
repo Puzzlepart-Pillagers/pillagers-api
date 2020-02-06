badc2ce3cf58e7cf0f9a4227ef31189053ab49f3
Kim Damsleth <kim@damsleth.com>
2020-02-06 17:06:33 +0100

    CORS

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,29 @@ import * as dotenv from "dotenv"
 dotenv.config()
 import endpoints from "./endpoints"
 import * as restify from "restify"
+import corsMiddleware from "restify-cors-middleware"
+
+
+const cors = corsMiddleware({
+    origins: ['*'],
+    allowHeaders: ["get","post","put","options"],
+    exposeHeaders: ['*']
+  })
 
 // Bootstrapping
 const server = restify.createServer({
     name: 'pillagers-api',
     version: '1.0.0'
 })
+
+server.pre(cors.preflight)
+server.pre(restify.plugins.pre.dedupeSlashes())
+server.use(cors.actual)
 server.use(restify.plugins.acceptParser(server.acceptable))
 server.use(restify.plugins.queryParser())
 server.use(restify.plugins.bodyParser())
 server.use(restify.plugins.jsonBodyParser())
-server.pre(restify.plugins.pre.dedupeSlashes())
+
 
 // Load all endpoints
 endpoints.initialize()
